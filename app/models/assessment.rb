0100aee36c56780b3647f53150be62583674abf8
RobertoJofreGodoy <roberto.jofre.godoy@gmail.com>
2018-11-11 12:42:16 -0300

    add validations to the assessment model

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -3,4 +3,9 @@ class Assessment < ApplicationRecord
   belongs_to :client
   belongs_to :owner
   has_many :properties
+
+  
+  validates :number_assesment, :state_id, :location, :habitant, :client_id, :owner_id, :start_date, :end_date, presence: true
+  validates :number_assesment, numericality: { greater_than: 0 }
+  validates :inhabited, inclusion: { in: [true, false] }
 end
