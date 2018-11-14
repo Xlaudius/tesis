7c11fdb72c59910a02a014a629ee14766ad476d9
alexis eduardo neira cruz <alexis.neira.cruz@gmail.com>
2018-11-14 20:00:56 -0300

    current_user and logged_in? helpers added on aplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user])
+    end
+  
+    def logged_in?
+         current_user != nil
+    end
+
 end
