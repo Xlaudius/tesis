49e7f958248d7064818bf65949a0e3c86517d20f
Xlaudius <claudio.rivera.sh@gmail.com>
2018-11-17 22:40:00 -0300

    [WIP] assessment spec

--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Assessment, type: :model do
-  describe "validations" do
+  describe "A basic assessment" do
     it "requires state_id to be set" do
-      expect(subject).to exist
-      expect(subject.errors[:state_id].value).to gt(1)&&lt(7)
+      expect(Assessment.new(state_id: 1)).to be_valid
+    end 
+  end
 end
