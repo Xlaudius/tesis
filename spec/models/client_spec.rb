cb1fe7b45329b8f97e3610277c3c95786f73cf2a
Xlaudius <claudio.rivera.sh@gmail.com>
2018-11-18 23:36:20 -0300

    Client Test

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,28 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe Client, type: :model do
+RSpec.describe Client, :type => :model do
+  let(:client_type) { ClientType.create(name: 'Particular') }
+  subject { 
+    described_class.new(
+      name: 'Client Name',
+      contact: 'Contact Name',
+      number: 988887777,
+      available: true,
+      client_type_id: client_type.id,
+      created_at: DateTime.now,
+      updated_at: DateTime.now + 1.day
+    )}
+  
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
 
-  let(:client_type) { ClientType.first}
-  let(:client) { Client.first }
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
+  it 'is not valid without a contact' do
+    subject.contact = nil
+    expect(subject).to_not be_valid
+  end
 
-  describe "A basic assessment" do
+  it 'is not valid without a number' do
+    subject.number = nil
+    expect(subject).to_not be_valid
+  end
 
-    it "has customer with basic information set" do
-      expect( Client.new( \
-        name: client.name, \
-        contact: client.contact, \
-        number: client.number, \
-        available: client.available, \
-        client_type_id: client_type.id \
-      )).to be_valid
-    end 
+  it 'is not valid without a client type' do
+    subject.client_type_id = nil
+    expect(subject).to_not be_valid
+  end
 
-    it "has information about the customer type" do
-      expect(ClientType.new( \
-        name: client_type \
-      )).to be_valid
-    end 
-
+  it 'has a boolean type available field' do
+    expect(subject.available).to be_in([true, false])
   end
 end
