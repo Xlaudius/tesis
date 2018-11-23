d878ac4769462d622cb4c4d28a7452a44c165f5e
Xlaudius <claudio.rivera.sh@gmail.com>
2018-11-23 00:05:55 -0300

    [CLIENT] deleted test comment

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,7 +3,6 @@ class Client < ApplicationRecord
   has_many :assessments
   
   validates_presence_of :name, :contact, :number, :available, :client_type_id
-  # validates :name, :contact, :number, :available, :client_type_id, presence: true
   validates :number, :client_type_id, numericality: { greater_than: 0 }
   validates :available, inclusion: { in: [true, false] }
 end
