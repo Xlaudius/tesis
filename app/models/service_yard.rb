b3d9d37a7fc26223343d0975aabb59d21437b076
FelipeSepulvedaRozas <felipe.s.rozas@gmail.com>
2018-11-22 21:08:34 -0300

    Carrierware Uploads files

--- app/models/service_yard.rb
+++ app/models/service_yard.rb
@@ -1,8 +1,8 @@
 class ServiceYard < ApplicationRecord
-  mount_uploader :img, ImageUploader
+  mount_uploaders :attachments, ImageUploader
 
   belongs_to :property
 
-  validates :img, :mts2, :property_id, presence: true
-	validates :mts2, :property_id, numericality:{ greater_than: 0 }
+  #validates :img, :mts2, :property_id, presence: true
+	#validates :mts2, :property_id, numericality:{ greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_233700) do
+ActiveRecord::Schema.define(version: 2018_11_22_195249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -644,6 +644,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_233700) do
     t.bigint "property_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "attachments"
     t.index ["property_id"], name: "index_service_yards_on_property_id"
   end
 
