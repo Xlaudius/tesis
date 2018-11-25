a61b7862b199d699854419332b4821ca5d6e1204
Xlaudius <claudio.rivera.sh@gmail.com>
2018-11-25 17:38:30 -0300

    client model test

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -4,43 +4,32 @@ RSpec.configure do |config|
 end
 
 RSpec.describe Client, :type => :model do
-  let(:client_type) { ClientType.create(name: 'Particular') }
-  subject { 
-    described_class.new(
-      name: 'Client Name',
-      contact: 'Contact Name',
-      number: 988887777,
-      available: true,
-      client_type_id: client_type.id,
-      created_at: DateTime.now,
-      updated_at: DateTime.now + 1.day
-    )}
+  context 'validation tests' do
+    let(:client_type) { build(:client_type) }
+    let(:client) { build(:client) }
+    
+    it 'is not valid without a name' do
+      client.name = nil
+      expect(client.save).to eq(false)
+    end
   
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a contact' do
-    subject.contact = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a number' do
-    subject.number = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a client type' do
-    subject.client_type_id = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'has a boolean type available field' do
-    expect(subject.available).to be_in([true, false])
+    it 'is not valid without a contact' do
+      client.contact = nil
+      expect(client.save).to eq(false)
+    end
+  
+    it 'is not valid without a number' do
+      client.number = nil
+      expect(client.save).to eq(false)
+    end
+  
+    it 'is not valid without a client type' do
+      client.client_type_id = nil
+      expect(client.save).to eq(false)
+    end
+  
+    it 'has a boolean type available field' do
+      expect(client.available).to be_in([true, false])
+    end
   end
 end
