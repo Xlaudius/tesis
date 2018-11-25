2479ea9ba6174d0b3f0050cb89117fb3b2c0ee07
Xlaudius <claudio.rivera.sh@gmail.com>
2018-11-25 18:08:14 -0300

    removed static assignations, only dynamic fixtures from this point on

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,11 +1,4 @@
 FactoryBot.define do
-  # factory :client do
-  #   name 'John Doe'
-  #   contact 'Jane Doe'
-  #   number 988887777
-  #   available true
-  #   client_type_id 1
-  # end
 
   factory :client, class: Client do
     name { Faker::Zelda.character }
@@ -14,4 +7,5 @@ FactoryBot.define do
     available { Faker::Boolean.boolean }
     client_type_id { [1, 2].sample }
   end
+  
 end
