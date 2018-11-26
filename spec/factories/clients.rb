328d4016a1384ae85cb519a4d15c0c9a67e7afac
Xlaudius <claudio.rivera.sh@gmail.com>
2018-11-26 00:32:25 -0300

    Clients associations on models

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -5,7 +5,9 @@ FactoryBot.define do
     contact { Faker::WorldOfWarcraft.hero }
     number { Faker::Number.number(8) }
     available { Faker::Boolean.boolean }
-    #client_type_id { [1, 2].sample }
+    
+    #Associations
+    client_type
 
   end
   
